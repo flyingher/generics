f2cd611ad2ff144aa568f2f2fcf61cb2a3f24d67
flyingher <springsource@yeah.net>
2012-11-10 23:58:04 +0800

    first commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+this is for generic studying!
---
+++ src/main/java/com/flyingh/generics/App.java
@@ -0,0 +1,13 @@
+package com.flyingh.generics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
